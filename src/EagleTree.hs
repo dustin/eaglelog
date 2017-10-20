54e0913100c78d13f5c92e009a3f28c6861936ec
dustin <dustin@spy.net>
2017-10-20 23:56:13 +0000

    Minor refactoring to move some helper functions to where they're used

--- src/EagleTree.hs
+++ src/EagleTree.hs
@@ -26,24 +26,23 @@ instance Show Session where
   show (Session n cn cv) =
     n ++ " cols=" ++ show cn ++ ", " ++ show (length cv) ++ " readings"
 
-cleanLines = map (BL.filter ('\r' /=)) . BL.lines
-
-readbs :: Read a => [BL.ByteString] -> Int -> a
-readbs l i = read $ BL.unpack $ l !! i
-
-
-parseHeaders :: [BL.ByteString] -> ([SessionHeader], [BL.ByteString])
-parseHeaders = let go h l@(a:b:rest) =
-                     let nums = BL.words b
-                         name = a in
-                       if length nums == 2 then
-                         go (SessionHeader(readbs nums 0, readbs nums 1, BL.unpack name):h) rest
-                       else (reverse h, l) in
-                 go []
-
 parseLog :: BL.ByteString -> [Session]
 parseLog f = let l = dropWhile (\l -> BL.unpack l /= "All Sessions") $ cleanLines f
                  (shdr, rest) = parseHeaders (drop 2 l)
                  fields = Map.fromList $ zip (map BL.unpack $ BL.words $ head rest) [0..]
                  readings = map BL.unpack $ tail rest in
                map (\ (SessionHeader (f, t, n)) -> Session n fields $ take (t-f) . drop f $ readings) shdr
+  where
+    parseHeaders :: [BL.ByteString] -> ([SessionHeader], [BL.ByteString])
+    parseHeaders = let go h l@(a:b:rest) =
+                         let nums = BL.words b
+                             name = a in
+                           if length nums == 2 then
+                             go (SessionHeader(readbs nums 0, readbs nums 1, BL.unpack name):h) rest
+                           else (reverse h, l) in
+                     go []
+
+    readbs :: Read a => [BL.ByteString] -> Int -> a
+    readbs l i = read $ BL.unpack $ l !! i
+
+    cleanLines = map (BL.filter ('\r' /=)) . BL.lines
