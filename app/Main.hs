f0efc086ca6ee14a0387d6a85b0f79740743d891
dustin <dustin@spy.net>
2017-10-22 20:43:30 +0000

    Started a bit of main.

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,18 @@ module Main where
 
 import EagleTree
 
+import Control.Monad (forM_)
+import System.IO
+import System.Environment
+import qualified Data.ByteString.Lazy as BL
+
+writeResults :: Int -> Session -> IO ()
+writeResults id sess = withFile ("session_" ++ show id ++ ".csv") WriteMode wr
+  where wr h = pure ()
+
 main :: IO ()
-main = putStrLn "hi"
+main = do
+  args <- getArgs
+  let (file:fields) = args
+  etdata <- BL.readFile file
+  forM_ (zip [1..] (parseLog etdata)) $ uncurry writeResults
