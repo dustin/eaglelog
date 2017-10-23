b56d83f046356b810ee34e2f85e13ab950a0bb09
dustin <dustin@spy.net>
2017-10-23 05:55:53 +0000

    Added some benchmarks

---
+++ test/Bench.hs
@@ -0,0 +1,20 @@
+module Main (main) where
+
+import EagleTree
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+import Criterion
+import Criterion.Main
+
+
+benchStringColumn logData = bench "string column" $ whnf (column id "PackVolt*100") logData
+benchIntColumn logData    = bench "int column" $ whnf (intColumn "PackVolt*100") logData
+benchGPSData logData      = bench "gps data" $ whnf gpsData logData
+
+main = do
+  ld <- BL.readFile "test/sample.FDR"
+  let stuff = last (parseLog ld)
+  defaultMain $ map ($ stuff) [benchStringColumn, benchIntColumn, benchGPSData]
