7f12162625fcee57b5483bd5e3b027cdbd91fecd
dustin <dustin@spy.net>
2017-10-25 00:15:46 +0000

    Couple hlint fixes in main

--- app/Main.hs
+++ app/Main.hs
@@ -11,13 +11,13 @@ import qualified Data.ByteString.Char8 as BC
 import qualified Data.Vector as V
 
 writeResults :: Int -> Session -> IO ()
-writeResults id sess = withFile ("session_" ++ show id ++ ".csv") WriteMode wr
+writeResults i sess = withFile ("session_" ++ show i ++ ".csv") WriteMode wr
   where wr h = (BL.hPut h) . encodeByName names $ rows sess
         names = V.fromList . map BC.pack $ colNames sess
 
 main :: IO ()
 main = do
   args <- getArgs
-  let (file:fields) = args
+  let (file:_fields) = args
   etdata <- BL.readFile file
   forM_ (zip [1..] (parseLog etdata)) $ uncurry writeResults
