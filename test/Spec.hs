94e9278b4da21bfa2411862f73ed6571fff94f57
dustin <dustin@spy.net>
2018-04-11 05:16:10 +0000

    Test cleanups

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,14 +10,17 @@ import Data.Csv (encode, encodeByName)
 
 import EagleTree
 
-testStringColumn :: [Session] -> Assertion
-testStringColumn l = assertEqual "" (Right (replicate 3 "1486")) $ take 3 <$> column id "PackVolt*100" (last l)
+columnTest :: (Show t, Eq t) => [Session] -> (String -> Session -> Either String [t]) -> String -> [t] -> Assertion
+columnTest l f k want = assertEqual "" (Right want) $ take 3 <$> f k (last l)
 
 testIntColumn :: [Session] -> Assertion
-testIntColumn l = assertEqual "" (Right (replicate 3 1486)) $ take 3 <$> intColumn "PackVolt*100" (last l)
+testIntColumn l = columnTest l intColumn "PackVolt*100" (replicate 3 1486)
 
 testFloatColumn :: [Session] -> Assertion
-testFloatColumn l = assertEqual "" (Right (replicate 3 0.0)) $ take 3 <$> floatColumn "GPSDist" (last l)
+testFloatColumn l = columnTest l floatColumn "GPSDist" (replicate 3 0.0)
+
+testStringColumn :: [Session] -> Assertion
+testStringColumn l = columnTest l (column id) "PackVolt*100" (replicate 3 "1486")
 
 testGPSData :: [Session] -> Assertion
 testGPSData l = assertEqual "" (replicate 3 ETGPSData {gpsLat = 36.988275
@@ -26,20 +29,20 @@ testGPSData l = assertEqual "" (replicate 3 ETGPSData {gpsLat = 36.988275
                                                       , gpsSpeed = 0
                                                       , gpsCourse = 273.2
                                                       , gpsDist = 0
-                                                      , gpsNumSats = 15} ) $ take 3 $ gpsData (last l)
+                                                      , gpsNumSats = 15} ) $ (take 3 . gpsData . last) l
 
 testColNames :: [Session] -> Assertion
-testColNames l = assertEqual "" ["Milliseconds", "IsEvent", "EventError"] $ take 3 $ colNames (last l)
+testColNames l = assertEqual "" ["Milliseconds", "IsEvent", "EventError"] $ (take 3 . colNames . last) l
 
 testRows :: [Session] -> Assertion
-testRows l = assertEqual "" 291 $ length.rows $ last l
+testRows l = assertEqual "" 291 $ (length.rows.last) l
 
 testCSVRecord :: [Session] -> Assertion
-testCSVRecord l = assertEqual "" x $ BL.unpack.encode.take 1.rows $ last l
+testCSVRecord l = assertEqual "" x $ (BL.unpack.encode.take 1.rows.last) l
   where x = "443700,1,50,3,50,49,50,254,50,50,0,15,1486,30,0,6,98,37,20,520,94,4857,500,1189,0,36.988275000000,-122.065895000000,166.0,0.0,273.2,0.0,77707100.0000,15,7,0,0,0,0,0,0,314,-8,6,-46,50,49,50,254,254,49,50,156,\"06/21/2017,13:35:07.10\",7\r\n"
 
 testCSVNamedRecord :: [Session] -> Assertion
-testCSVNamedRecord l = assertEqual "" x $ BL.unpack.encodeByName (v ["Milliseconds", "IsEvent", "EventError"]).take 1.rows $ last l
+testCSVNamedRecord l = assertEqual "" x $ (BL.unpack.encodeByName (v ["Milliseconds", "IsEvent", "EventError"]).take 1.rows.last) l
   where v = V.fromList . map BC.pack
         x = "Milliseconds,IsEvent,EventError\r\n443700,1,50\r\n"
 
@@ -54,7 +57,7 @@ tests = map (\(name, fun) -> testCase name (td fun)) [
   ("csv record", testCSVRecord),
   ("csv named record", testCSVNamedRecord)
   ]
-  where td f = BL.readFile "test/sample.FDR" >>= \d -> f (parseLog d)
+  where td f = BL.readFile "test/sample.FDR" >>= f . parseLog
 
 main :: IO ()
 main = defaultMain $ testGroup "All Tests" tests
